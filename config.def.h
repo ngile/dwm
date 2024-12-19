2aff3ef503ea7686678894db25077e4a3a4a646d
Jenkins <jenkins@backseatgaming.com>
2024-12-19 21:15:39 +0200

    Update colors

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Cousine Nerd Font:size=10" };
-static const char dmenufont[]       = "Cousine Nerd Font:size=10";
+static const char *fonts[]          = { "CousineNerdFont:size=10" };
+static const char dmenufont[]       = "CousineNerdFont:size=10";
 //background color
 static const char col_gray1[]       = "#292522";
 //inactive windows border color
@@ -14,9 +14,9 @@ static const char col_gray2[]       = "#ECE1D7";
 //font color
 static const char col_gray3[]       = "#ECE1D7";
 //current tag and current window font color
-static const char col_gray4[]       = "#ECE1D7";
+static const char col_gray4[]       = "#7F91B2";
 //topbar second color and active window border color
-static const char col_cyan[]        = "#4053435";
+static const char col_cyan[]        = "#34302C";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -24,7 +24,8 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "󰲠"", "󰲢", "󰲤", "󰲦", "󰲨", "󰲪", "󰲬" };
+// static const char *tags[] = { "󰲠", "󰲢", "󰲤", "󰲦", "󰲨", "󰲪", "󰲬" };
+static const char *tags[] = { "1", "2", "3", "4", "5" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -65,7 +66,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "wezterm", NULL };
-
+static const char *flameshot[] = { "flameshot", "gui", NULL };
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
