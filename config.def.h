1f6c2bb1ae46ca191d0d0092368f9a3cb40db13b
Jenkins <jenkins@backseatgaming.com>
2024-12-19 21:46:31 +0200

    Disable dmenu

--- config.def.h
+++ config.def.h
@@ -65,15 +65,17 @@ static const Layout layouts[] = {
 /* commands */
 static const char *rofid[]     = { "rofi", "-show", "drun", "-show-icons", "-font Cousine Nerd Font 10", NULL };
 static const char *rofi[]     = { "rofi", "-show", "run", "-font Cousine Nerd Font 10", NULL };
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+// static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+// static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "wezterm", NULL };
 static const char *flameshot[] = { "flameshot", "gui", NULL };
+static const char *rofipass[] = { "rofipass", "gui", NULL };
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = rofid } },
 	{ MODKEY|ShiftMask,             XK_d,      spawn,          {.v = rofi } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_p, spawn,          {.v = rofipass } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
