47313315dcf60abf6f3dac56ce818a91a8245881
Georgi Kostadinov <georgi.kostadinov@adata.pro>
2026-01-06 15:34:34 +0200

    Added dwm-swallow-6.3.diff

--- config.def.h
+++ config.def.h
@@ -18,6 +18,7 @@ static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int topbar             = 1;        /* 0 means bottom bar */
 // static const char *fonts[]          = { "Iosevka Curly:size=10" };
 // static const char *fonts[]          = { "Iosevka Slab Expanded:size=10" };
@@ -46,13 +47,11 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Chromium",     NULL,       NULL,       1 << 3,            0,           -1 },
-	{ "mpv",  NULL,       NULL,       1 << 5,       0,           -1 },
-	{ "firefox",  NULL,       NULL,       1 << 3,       0,           -1 },
-	{ "Slack",  NULL,       NULL,       1 << 6,       0,           -1 },
-	{ "Viber",  NULL,       NULL,       1 << 6,       0,           -1 },
-	{ "google-chrome",  NULL,       NULL,       1 << 2,       0,           -1 },
+	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
+	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ TERMINAL,      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
